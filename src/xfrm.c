RoadRunnr/umip

commit 454c91bf7876ba2d45b21b483e1a862d2e7e764b

[BUGFIX] Avoid sending ESP-protected NS/NA
With IPsec enabled in tunnel mode for payload packets, NS/NA which
destination address is global and targetting the MN or HA were
encrypted in ESP tunnel mode because the bypass XFRM policy for
NS/NA is of type SUB: the IPsec tunnel one being of type MAIN,
it is applied anyway, whatever its priotity.

This commit fixes this behaviour by modifying the priority
of the IPsec tunnel policies and by using MAIN policies for
the NS/NA bypass policies (there is actually no real reasons
to use SUB ones, as we want the packets to be untouched anyway).

Signed-off-by: Romain Kuntz <r.kuntz@ipflavors.com>

src/xfrm.c
@@ -1132,8 +1132,8 @@ static int xfrm_cn_init(void)
 	/* Let Neighbor Solicitation messages bypass bindings */
 	set_selector(&in6addr_any, &in6addr_any,
 		     IPPROTO_ICMPV6, ND_NEIGHBOR_SOLICIT, 0, 0, &sel);
-	if (xfrm_mip_policy_add(&sel, 0, XFRM_POLICY_OUT, XFRM_POLICY_ALLOW,
-				MIP6_PRIO_NO_RO_SIG_ANY, NULL, 0) < 0)
+	if (xfrm_ipsec_policy_add(&sel, 0, XFRM_POLICY_OUT, XFRM_POLICY_ALLOW,
+				  MIP6_PRIO_NO_RO_SIG_ANY, NULL, 0) < 0)
 		return -1;
 
 	/*
@@ -1143,8 +1143,8 @@ static int xfrm_cn_init(void)
 	 */
 	set_selector(&in6addr_any, &in6addr_any,
 		     IPPROTO_ICMPV6, ND_NEIGHBOR_ADVERT, 0, 0, &sel);
-	if (xfrm_mip_policy_add(&sel, 0, XFRM_POLICY_OUT, XFRM_POLICY_ALLOW,
-				MIP6_PRIO_HOME_SIG_ANY, NULL, 0) < 0)
+	if (xfrm_ipsec_policy_add(&sel, 0, XFRM_POLICY_OUT, XFRM_POLICY_ALLOW,
+				  MIP6_PRIO_HOME_SIG_ANY, NULL, 0) < 0)
 		return -1;
 
 	/* Let ICMPv6 error messages bypass bindings */
@@ -1171,11 +1171,11 @@ static void xfrm_cn_cleanup(void)
 
 	set_selector(&in6addr_any, &in6addr_any,
 		     IPPROTO_ICMPV6, ND_NEIGHBOR_SOLICIT, 0, 0, &sel);
-	xfrm_mip_policy_del(&sel, XFRM_POLICY_OUT);
+	xfrm_ipsec_policy_del(&sel, XFRM_POLICY_OUT);
 
 	set_selector(&in6addr_any, &in6addr_any,
 		     IPPROTO_ICMPV6, ND_NEIGHBOR_ADVERT, 0, 0, &sel);
-	xfrm_mip_policy_del(&sel, XFRM_POLICY_OUT);
+	xfrm_ipsec_policy_del(&sel, XFRM_POLICY_OUT);
 
 	set_selector(&in6addr_any, &in6addr_any,
 		     IPPROTO_ICMPV6, 0, 0, 0, &sel);

src/ipsec.c
@@ -140,7 +140,7 @@ static void _set_sp(struct xfrm_userpolicy_info *sp,
 		sp->priority = MIP6_PRIO_RO_SIG_RR;
 		break;
 	case IPSEC_POLICY_TYPE_TUNNELPAYLOAD:
-		sp->priority = MIP6_PRIO_RO_SIG_RR;
+		sp->priority = MIP6_PRIO_NO_RO_DATA;
 		break;
 
 		/* Transport */

commit 2b352c5480f8ad14b245c912f7d748bf56151a69

[BUGFIX] Fix NS not being sent on the home link
Commit 454c91bf787 has introduced an issue: MN could not send NS
from the Home Link anymore. Using a MAIN policy for the NS/NA bypass
policy would make the SUB block policy (upon BU/BA exchange) to be
enforced. This commit fixes the issue by installing both a MAIN and
SUB bypass policy of higher preference.

Using a MAIN block policy instead of the SUB one is not a solution
since it would block the sending of BUs.

Signed-off-by: Romain Kuntz <r.kuntz@ipflavors.com>

src/xfrm.c
@@ -1116,8 +1116,9 @@ static int xfrm_cn_init(void)
 
 	XDBG("Adding policies and states for CN\n");
 
-	/* Create policy for all BUs with home flag NOT set to 
-	   use home address option */
+	/* Create policy for all BUs with home flag NOT set to
+	 * use home address option. 
+	 */
 	if (cn_wildrecv_bu_pol_add())
 		return -1;
 
@@ -1129,20 +1130,30 @@ static int xfrm_cn_init(void)
 	if (xfrm_cn_policy_mh_out_touch(0) < 0)
 		return -1;
 
-	/* Let Neighbor Solicitation messages bypass bindings */
+	/* Let Neighbor Solicitation messages bypass bindings.
+	 * We add both a MAIN and SUB policy in order to bypass the 
+	 * SUB block and the potential MAIN IPsec policies.
+	 */
 	set_selector(&in6addr_any, &in6addr_any,
 		     IPPROTO_ICMPV6, ND_NEIGHBOR_SOLICIT, 0, 0, &sel);
+	if (xfrm_mip_policy_add(&sel, 0, XFRM_POLICY_OUT, XFRM_POLICY_ALLOW,
+				MIP6_PRIO_NO_RO_SIG_ANY, NULL, 0) < 0)
+		return -1;
 	if (xfrm_ipsec_policy_add(&sel, 0, XFRM_POLICY_OUT, XFRM_POLICY_ALLOW,
 				  MIP6_PRIO_NO_RO_SIG_ANY, NULL, 0) < 0)
 		return -1;
 
-	/*
-	 * Let Neighbor Advertisement messages bypass bindings 
+	/* Let Neighbor Advertisement messages bypass bindings
 	 * This policy is high priority(priory 3) not to block 
-	 * by the BlockPolicy during registration.
+	 * by the BlockPolicy during registration. We add both a 
+	 * MAIN and SUB policy in order to bypass the SUB block 
+	 * and the potential MAIN IPsec policies.
 	 */
 	set_selector(&in6addr_any, &in6addr_any,
 		     IPPROTO_ICMPV6, ND_NEIGHBOR_ADVERT, 0, 0, &sel);
+	if (xfrm_mip_policy_add(&sel, 0, XFRM_POLICY_OUT, XFRM_POLICY_ALLOW,
+				MIP6_PRIO_HOME_SIG_ANY, NULL, 0) < 0)
+		return -1;
 	if (xfrm_ipsec_policy_add(&sel, 0, XFRM_POLICY_OUT, XFRM_POLICY_ALLOW,
 				  MIP6_PRIO_HOME_SIG_ANY, NULL, 0) < 0)
 		return -1;
@@ -1171,10 +1182,12 @@ static void xfrm_cn_cleanup(void)
 
 	set_selector(&in6addr_any, &in6addr_any,
 		     IPPROTO_ICMPV6, ND_NEIGHBOR_SOLICIT, 0, 0, &sel);
+	xfrm_mip_policy_del(&sel, XFRM_POLICY_OUT);
 	xfrm_ipsec_policy_del(&sel, XFRM_POLICY_OUT);
 
 	set_selector(&in6addr_any, &in6addr_any,
 		     IPPROTO_ICMPV6, ND_NEIGHBOR_ADVERT, 0, 0, &sel);
+	xfrm_mip_policy_del(&sel, XFRM_POLICY_OUT);
 	xfrm_ipsec_policy_del(&sel, XFRM_POLICY_OUT);
 
 	set_selector(&in6addr_any, &in6addr_any,